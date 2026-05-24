3931eccca0d873699b8210729fc1fd822622f457
Jeremy Brown <jeremybrownpei@gmail.com>
2026-05-24 19:40:20 -0300

    Implement timer start functionality in start-timer.ts
    This script starts a timer by logging the current date and time, inserts a new row in the worksheet, and copies relevant data from specified cells.

---
+++ start-timer.ts
@@ -0,0 +1,35 @@
+
+function main(workbook: ExcelScript.Workbook) {
+    //Get Sheet
+    let sheet = workbook.getWorksheet("Sheet1");
+
+    // Get current date time for Stars and possible ends
+    let now = new Date();
+
+    //Check for an "active" (exntry without and end time) entry C6
+    let activeEndCell = sheet.getRange("C6");
+    if (activeEndCell.getValueTypes()[0][0] === ExcelScript.RangeValueType.empty) {
+        //Set previous 
+        activeEndCell.setValue(now.toLocaleString());
+    }
+
+
+    //Insert a new row    
+    //sheet.getRange("A6").select();
+    //let selectedRange = workbook.getActiveCell();
+    let selectedRange = sheet.getRange('A6');
+    selectedRange.getEntireRow().insert(ExcelScript.InsertShiftDirection.down);
+
+    // Get the source cell and target cell
+    let sourceCellDescription = workbook.getWorksheet("Sheet1").getRange("A2");
+    let targetCellDescription = workbook.getWorksheet("Sheet1").getRange("A6");
+    // Copy the value from the source to the target
+    targetCellDescription.setValue(sourceCellDescription.getValue());
+
+       
+    sheet.getRange("B6").setValue(now.toLocaleString());
+
+    // Copy the start new instance button
+    let startNewInstanceBtn = sheet.getRange("E1").getValue();
+    sheet.getRange("E6").setValue(startNewInstanceBtn);
+}
