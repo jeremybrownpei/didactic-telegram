7073bb9cde3d35bed3a28d9de37b2036ba4c25f5
Jeremy Brown <jeremybrownpei@gmail.com>
2026-05-24 22:47:45 -0300

    Use Excel date format instead of local date string
    Replaced local date string with Excel date format for cell values.

--- start-timer.ts
+++ start-timer.ts
@@ -1,16 +1,17 @@
-
 function main(workbook: ExcelScript.Workbook) {
     //Get Sheet
     let sheet = workbook.getWorksheet("Sheet1");
 
     // Get current date time for Stars and possible ends
     let now = new Date();
+    const excelDate = (now.getTime() / (24 * 60 * 60 * 1000)) + 25569.0;
 
     //Check for an "active" (exntry without and end time) entry C6
     let activeEndCell = sheet.getRange("C6");
     if (activeEndCell.getValueTypes()[0][0] === ExcelScript.RangeValueType.empty) {
         //Set previous 
-        activeEndCell.setValue(now.toLocaleString());
+        //activeEndCell.setValue(now.toLocaleString());
+        activeEndCell.setValue(excelDate);
     }
 
 
@@ -27,6 +28,9 @@ function main(workbook: ExcelScript.Workbook) {
     targetCellDescription.setValue(sourceCellDescription.getValue());
 
        
-    sheet.getRange("B6").setValue(now.toLocaleString());
+    sheet.getRange("B6").setValue(excelDate);
 
+    // Copy the start new instance button
+    let startNewInstanceBtn = sheet.getRange("E1").getValue();
+    sheet.getRange("E6").setValue(startNewInstanceBtn);
 }
