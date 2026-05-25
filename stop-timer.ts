7b1205d8c511d55ef60f170db4fc41bc409d9eaf
Jeremy Brown <jeremybrownpei@gmail.com>
2026-05-24 22:47:29 -0300

    Update active end time to use Excel date

--- stop-timer.ts
+++ stop-timer.ts
@@ -5,12 +5,13 @@ function main(workbook: ExcelScript.Workbook) {
 
   // Get current date time for Stars and possible ends
   let now = new Date();
+  const excelDate = (now.getTime() / (24 * 60 * 60 * 1000)) + 25569.0;
 
   //Check for an "active" (exntry without and end time) entry C6
   let activeEndCell = sheet.getRange("C6");
   if (activeEndCell.getValueTypes()[0][0] === ExcelScript.RangeValueType.empty) {
     //Set previous 
-    activeEndCell.setValue(now.toLocaleString());
+    activeEndCell.setValue(excelDate);
   }
 
   //clear timer description
